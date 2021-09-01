724a2cbcd8de7681836150cbac4dcb543e1ad61e
Flavio Amancio <amancio.flavio@yahoo.com.br>
2021-09-01 17:04:26 -0300

    Correção par ou ímpar.

--- src/alelo/ParouImpar01.java
+++ src/alelo/ParouImpar01.java
@@ -6,11 +6,11 @@ public class ParouImpar01 {
     public static void main (String [] args) {
         Scanner in = new Scanner(System.in);
 
-        System.out.println("Digite um número para verificar se é par ou ímpar: ");
+        System.out.print("Digite um número para verificar se é par ou ímpar: ");
         double num = in.nextInt();
 
 
-        if (num == num /2) {
+        if (num % 2 == 0) {
             System.out.println("O número digitado é par!");
         }   else {
             System.out.println("O número informado é ímpar!");
